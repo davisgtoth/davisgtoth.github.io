cab3a1dd9cf532abd1c434bfb7d3ea805b7763b1
davistoth <davisgtoth@gmail.com>
2024-11-22 23:02:57 -0800

    added clock videos and hid project.rpt

--- javascript/commands.js
+++ javascript/commands.js
@@ -1,29 +1,3 @@
-// // JavaScript to toggle the visibility of the description
-// // const clickableText = document.querySelector('.command');
-// // const description = document.getElementById('command-output');
-// const clickableElements = document.querySelectorAll('.command');
-// const hiddenContents = document.querySelectorAll('.command-output');
-
-// // clickableText.addEventListener('click', () => {
-// //     if (description.style.display === "none" || description.style.display === "") {
-// //         description.style.display = "block"; // Show the hidden content
-// //     } else {
-// //         description.style.display = "none"; // Hide it again
-// //     }
-// // });
-
-// clickableElements.forEach((clickable, index) => {
-//     clickable.addEventListener('click', () => {
-//         // Toggle display of the corresponding hidden content
-//         const correspondingContent = hiddenContents[index];
-//         if (correspondingContent.style.display === "none" || correspondingContent.style.display === "") {
-//             correspondingContent.style.display = "block";
-//         } else {
-//             correspondingContent.style.display = "none";
-//         }
-//     });
-// });
-
 // Select all elements with the class "command"
 const commandElements = document.querySelectorAll('.command');
 
